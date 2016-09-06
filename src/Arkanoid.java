c96b3f60c9e3b8a2f12ccd8c5d791c5b7f862898
Bernardo Costa de Araujo <3ernardo@gmail.com>
2016-09-06 04:51:57 -0300

    Game setup

--- src/Arkanoid.java
+++ src/Arkanoid.java
@@ -1,7 +1,25 @@
 import com.senac.SimpleJava.Console;
+import com.senac.SimpleJava.Graphics.Canvas;
+import com.senac.SimpleJava.Graphics.GraphicApplication;
 
-public class Arkanoid {
-	public void run(){
-		Console.println("Hello Arkanoid!");
+public class Arkanoid extends GraphicApplication {
+
+	@Override
+	protected void draw(Canvas canvas) {
+		// TODO Auto-generated method stub
+		
 	}
+
+	@Override
+	protected void setup() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void loop() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
